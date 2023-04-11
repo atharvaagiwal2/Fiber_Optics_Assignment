c1d583d4cb606afe4be9a5143c85ac2c3f7b1b47
atharvaagiwal2 <130425572+atharvaagiwal2@users.noreply.github.com>
2023-04-11 09:42:58 +0530

    Update Q1b.m

--- Q1b.m
+++ Q1b.m
@@ -5,9 +5,7 @@ f1 = @(V,b) -V.*sqrt(1-b).*cot(V.*sqrt(1-b))-V.*sqrt(b)
 fimplicit(f1,[0 10 0 1],'--')
 hold off
 legend('Symmetric Mode','Anti-symmetric Mode')
-title({'Variation of the normalized propagation parameter b', ...
- 'for TE modes with the normalized frequency V', ...
- 'for a planar waveguide'})
+title({'Variation of the normalized propagation parameter b','for TE modes with the normalized frequency V','for a planar waveguide'})
 grid on
 xlabel('V(rad)')
 ylabel('b')
